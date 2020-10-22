c6b342b526c352ce1e7dabe28687cee3854b3a67
Felipe Polato <fepolato@hotmail.com>
2020-10-22 12:04:05 -0300

    elemento produtos com styled components e osganizacao estatica das tags

--- src/components/produtos/Produtos.js
+++ src/components/produtos/Produtos.js
@@ -1,25 +1,36 @@
 import React from "react";
-import Card from "./Card"
-import styled from "styled-components"
+import Card from "./Card";
+import styled from "styled-components";
 
-const estoquePesquisa = styled.div`
-display: grid;
-justify-content:center;
-align-items:space-between;
-`
+const CardsContainer = styled.div`
+  display: grid;
+  justify-content: center;
+  align-items: center;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-columns: 1fr 1fr;
+  border: 1px solid black;
+`;
 
-function Produtos() {
-  return (
-  <estoquePesquisa>
-      <p>Estoque</p>
-      <select>
-          <option>Maior Preço</option>
-          <option>Menor Preço</option>
-      </select>
-      <Card/>
+const CardsFilter = styled.div``;
 
-  </estoquePesquisa>
-  )
+class Produtos extends React.Component {
+  render() {
+    return (
+      <CardsContainer>
+        <p>Estoque de produtos: 10</p>
+        <CardsFilter>
+          <label>
+            Filtrar:
+            <select>
+              <option>Maior preço</option>
+              <option>Menor preço</option>
+            </select>
+          </label>
+        </CardsFilter>
+        <Card />
+      </CardsContainer>
+    );
+  }
 }
 
 export default Produtos;
